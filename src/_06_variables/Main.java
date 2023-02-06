63e7183e43147ec6e9058eee007fa8eaa32b0f81
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:31:09 +0100

    variables

---
+++ src/_06_variables/Main.java
@@ -0,0 +1,12 @@
+package _06_variables;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = new String("This is a string");  // using a constructor
+        System.out.println(string + " ; ) ");
+
+        String name = "Daniel Ouattara"; // using a literal
+        System.out.println(name);
+
+    }
+}
