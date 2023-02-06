9b473aadd0442b7f5eb344f226dfa86b7a8ea2f5
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:32:18 +0100

    primitive Data types

---
+++ src/_09_primitives_data_types/Main.java
@@ -0,0 +1,20 @@
+package _09_primitives_data_types;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isPizzaDelicious = true;
+        String isPizzaDelicious2 = "true";
+    }
+}
+
+
+/*
+ boolean,
+ byte,
+ char,
+ short,
+ int ,
+ long,
+ float,
+ double
+*/
