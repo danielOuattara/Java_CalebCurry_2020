b3d536c3116725bbc0bead8df1dd2e5404a9e05a
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:31:31 +0100

    data types

---
+++ src/_07_data_types/Main.java
@@ -0,0 +1,11 @@
+package _07_data_types;
+
+public class Main {
+    public static void main(String[] args) {
+        // primitives types
+        int  x = 5;
+        //OR
+        Integer y = 5;  // autoboxed Integer variables
+
+    }
+}
