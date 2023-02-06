542cabf0848f5d4ec09a87704ed357e316f85648
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:31:53 +0100

    variables in depth

---
+++ src/_08_variables_in_depth/Main.java
@@ -0,0 +1,17 @@
+package _08_variables_in_depth;
+
+public class Main {
+    public static void main(String[] args) {
+        //------------
+        int x; // declaration
+        x = 5; // initialization
+        x = 10;
+        //------------
+        int y = 5;
+        //------------
+        Integer z = 5;
+
+
+        final int MONTHS_IN_YEAR = 12;
+    }
+}
