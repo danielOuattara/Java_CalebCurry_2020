d6a19f789cbd745c818287f66f541eb8d84610d9
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:30:52 +0100

    input output

--- src/_05_input_ouput/Main.java
+++ src/_05_input_ouput/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name : ");
+        System.out.print("Enter your name : ");
         String name = scanner.nextLine();
         System.out.println("Hello " +  name);
  }
