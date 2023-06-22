26be44d84d8e9225a8f0cdf08594fe10e701810b
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:06:21 +0200

    11 numeric data type & props

---
+++ src/_11_numeric_data_types_and_properties/Main.java
@@ -0,0 +1,18 @@
+package _11_numeric_data_types_and_properties;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        Integer y = 5;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Double.POSITIVE_INFINITY);
+        System.out.println(Double.NEGATIVE_INFINITY);
+        System.out.println(Double.NaN);
+        System.out.println(Math.sqrt(-1));
+        System.out.println(Double.isNaN(Math.sqrt(-1)));
+
+
+    }
+}
