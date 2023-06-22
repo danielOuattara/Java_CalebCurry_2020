fdc4914a71293dac5bceb9331fb8e283348696d0
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:07:02 +0200

    12 numeric expressions & operators

---
+++ src/_12_numeric_expression_and_operators/Main.java
@@ -0,0 +1,32 @@
+package _12_numeric_expression_and_operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int int_x = 5 / 2;
+        System.out.println(int_x);
+
+        double double_x = 5 / 2;
+        System.out.println(double_x);
+
+        double double_x_2 = 5 / 2.0;
+        System.out.println(double_x_2);
+
+        double double_x_3 = (double)5 / 2;
+        System.out.println(double_x_3);
+        /*
+        + addition
+        - subtraction
+        * multiplication
+        / division
+        % modulus
+        ++ increment : x++  ++x
+        -- decrement : x-- --x
+        +=
+        -=
+        *=
+        /=
+        %=
+
+        */
+    }
+}
