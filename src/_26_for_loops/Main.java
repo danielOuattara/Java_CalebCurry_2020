534252770d2b830401444ae408c15ac2596c94f4
danielOuattara <daniel.ouattara@gmx.com>
2023-07-19 12:36:22 +0200

    26 for loops

---
+++ src/_26_for_loops/Main.java
@@ -0,0 +1,18 @@
+package _26_for_loops;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Iterator - Comparison - Update
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" iterator: " + i);
+        }
+
+        System.out.println("-----------------------------");
+
+        for (int j = 10; j > 0; j--) {
+            System.out.println(" iterator: " + j);
+        }
+    }
+}
