7953cdfd6e5df156e5a7415a0013fcf8d9029662
danielOuattara <daniel.ouattara@gmx.com>
2023-07-24 21:22:45 +0200

    40 2D arrays

---
+++ src/_40_2D_arrays/Main.java
@@ -0,0 +1,17 @@
+package _40_2D_arrays;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[][] grades_1 = new int[3][2];
+        System.out.println("grades_1 = " + Arrays.deepToString(grades_1));
+        System.out.println("grades_1_Rows = " + Arrays.toString(grades_1[1]));
+        System.out.println("grades_1 = " + Arrays.deepToString(grades_1));
+
+        //{1, 2, 3, 77, 4, 5, 7, 9};
+    }
+}
