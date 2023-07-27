096069f730e6cfb7b5c231304cee81a00a3e4a17
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 21:41:35 +0200

    60 arguments & parameters

---
+++ src/_60_arguments_and_parameters/Main.java
@@ -0,0 +1,14 @@
+package _60_arguments_and_parameters;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+        user_1.firstName = "John";
+        user_1.lastName = "Doe";
+
+        user_1.output(3);
+
+    }
+}
---
+++ src/_60_arguments_and_parameters/User.java
@@ -0,0 +1,15 @@
+package _60_arguments_and_parameters;
+
+public class User {
+    public String firstName;
+    public String lastName;
+
+    public void output(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(firstName + " " + lastName);
+
+        }
+    }
+
+
+}
