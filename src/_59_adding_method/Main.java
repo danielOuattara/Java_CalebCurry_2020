566510a2a9a054da70c7ae570a2b2a47d2160bbf
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 21:41:05 +0200

    58 adding field; 59 adding method

---
+++ src/_59_adding_method/Main.java
@@ -0,0 +1,14 @@
+package _59_adding_method;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+        user_1.firstName = "John";
+        user_1.lastName = "Doe";
+
+        user_1.output();
+
+    }
+}
---
+++ src/_59_adding_method/User.java
@@ -0,0 +1,12 @@
+package _59_adding_method;
+
+public class User {
+    public String firstName;
+    public String lastName;
+
+    public void output() {
+        System.out.println(firstName + " " + lastName);
+    }
+
+
+}
