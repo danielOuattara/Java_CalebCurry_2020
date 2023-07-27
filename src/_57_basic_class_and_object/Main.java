9f62c5b883f2ca10c6026a16c1ed2b4d2c0c6d01
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 21:38:54 +0200

    57 basic class & object

---
+++ src/_57_basic_class_and_object/Main.java
@@ -0,0 +1,10 @@
+package _57_basic_class_and_object;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+
+    }
+}
---
+++ src/_57_basic_class_and_object/User.java
@@ -0,0 +1,5 @@
+package _57_basic_class_and_object;
+
+public class User {
+
+}
