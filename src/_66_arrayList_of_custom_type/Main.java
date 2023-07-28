ec09b8c0085fa8cb361ecfcf67eccc30b5c4da7a
danielOuattara <daniel.ouattara@gmx.com>
2023-07-28 23:42:35 +0200

    67: arrayList of custom type

---
+++ src/_66_arrayList_of_custom_type/Main.java
@@ -0,0 +1,14 @@
+package _66_arrayList_of_custom_type;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+        user_1.setFirstName("John");
+        user_1.setLastName("Doe");
+
+        System.out.println("FullName: " + user_1.getFullName());
+
+    }
+}
