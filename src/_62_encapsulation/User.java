c25e2901d94a1ec0271e84b6848d28bb2e3cb8d3
danielOuattara <daniel.ouattara@gmx.com>
2023-07-28 23:40:58 +0200

    62 encapsulation

---
+++ src/_62_encapsulation/User.java
@@ -0,0 +1,12 @@
+package _62_encapsulation;
+
+public class User {
+    public String firstName;
+    public String lastName;
+
+    public String output() {
+        return firstName + " " + lastName;
+    }
+
+
+}
