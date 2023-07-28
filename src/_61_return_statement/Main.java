d00c36164bb178a9ec55daff2a750eb94e3a1ccd
danielOuattara <daniel.ouattara@gmx.com>
2023-07-28 23:40:38 +0200

    61 return statm.

---
+++ src/_61_return_statement/Main.java
@@ -0,0 +1,15 @@
+package _61_return_statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+        user_1.firstName = "John";
+        user_1.lastName = "Doe";
+
+        String user_1FullName = user_1.output();
+        System.out.println("user_1FullName: " + user_1FullName);
+
+    }
+}
---
+++ src/_61_return_statement/User.java
@@ -0,0 +1,12 @@
+package _61_return_statement;
+
+public class User {
+    public String firstName;
+    public String lastName;
+
+    public String output() {
+        return firstName + " " + lastName;
+    }
+
+
+}
